a59af65ac1ff3848b0bf9ba4fce89b3ec8d2488b
ahmetbugrayasar <59139830+ahmetbugrayasar@users.noreply.github.com>
2022-07-18 12:53:02 +0300

    flyingdecision stuff

--- Assets/Script/FlyingDecision.cs
+++ Assets/Script/FlyingDecision.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 using Pathfinding;
 public class FlyingDecision : MonoBehaviour
 {
-    
-    
+    public Transform player;
+    public Transform goTransform;
+    private Vector3 plPosition;
+    private Vector3 goPosition;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<AIPath>().enabled = false;
-        
+        goTransform = gameObject.GetComponent<Transform>();
     }
-
+    private void FixedUpdate()
+    {
+        plPosition = player.position;
+        goPosition = transform.position;
+    }
     // Update is called once per frame
     void Update()
     {
+       
         
     }
 }
